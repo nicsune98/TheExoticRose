63c839db5956b360db3b31954c501455b3da4fad
nicsune98 <wcuascota123@gmail.com>
2023-05-23 07:54:18 -0500

    Uso GPS

--- TheExoticRose/Vistas/IngresoMapa.xaml.cs
+++ TheExoticRose/Vistas/IngresoMapa.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Geolocator;
 
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 using Xamarin.Forms.Xaml;
 
 namespace TheExoticRose.Vistas
@@ -15,6 +17,46 @@ namespace TheExoticRose.Vistas
         public IngresoMapa()
         {
             InitializeComponent();
+        }
+        private async void Localizar(double latitud, double longitud, string ubicacion)
+        {
+            Pin pin = new Pin()
+            {
+                Type = PinType.Place,
+                Label = ubicacion,
+                Position = new Position(latitud, longitud)
+            };
+            Mapa.Pins.Add(pin);
+            Mapa.MoveToRegion(MapSpan.FromCenterAndRadius(pin.Position, Distance.FromMeters(300)));
+
+        }
+        private async void btCapturar_Clicked(object sender, EventArgs e)
+        {
+            if (CrossGeolocator.Current.IsGeolocationAvailable)
+            {
+                if (CrossGeolocator.Current.IsGeolocationEnabled)
+                {
+                    var locator = CrossGeolocator.Current;
+                    locator.DesiredAccuracy = 50;
+
+                    var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
+
+                    Device.BeginInvokeOnMainThread(() => {
+                        latitude.Text = $"Latitud : {position.Latitude}";
+                        longitude.Text = $"Longitud : {position.Longitude}";
+
+                    });
+                    Localizar(position.Latitude, position.Longitude, "Ubicacion Actual");
+                }
+                else
+                {
+                    await DisplayAlert("Message", "GPS Not Enabled", "Ok");
+                }
+            }
+            else
+            {
+                await DisplayAlert("Message", "GPS Not Available", "Ok");
+            }
         }
     }
 }
